932449e04a6a6238d6dc1c732f5903784e99f38e
yuhansun <wdmzssyh@gmail.com>
2017-05-14 13:28:01 -0700

    ratio experiment (reverted from commit 69dd0564c2ef8d591c7e2b5b1d635522f9426401)

--- src/main/java/org/datasyslab/GeoReach/Config.java
+++ src/main/java/org/datasyslab/GeoReach/Config.java
@@ -1,83 +1,76 @@
 package org.datasyslab.GeoReach;
 
-public class Config { 
-
-	public Config() {}
-	
-	public static enum Distribution 
-	{
-		Random_spatial_distributed, Clustered_distributed, Zipf_distributed
-	}
-	private String SERVER_ROOT_URI = "http://localhost:7474/db/data";
-
-	private String longitude_property_name = "longitude";
-	private String latitude_property_name = "latitude";
-
-	private String RMBR_minx_name = "RMBR_minx";
-	private String RMBR_miny_name = "RMBR_miny";
-	private String RMBR_maxx_name = "RMBR_maxx";
-	private String RMBR_maxy_name = "RMBR_maxy";
-
-	private String GeoB_name = "GeoB";
-	private String bitmap_name = "Bitmap";
-	private int merge_ratio = 0;
-	private double Max_RMBR_ratio = 2.0D;
-	private int Max_ReachGrid = 200;
-
-	public String GetServerRoot() {
-		return SERVER_ROOT_URI;
-	}
-
-	public String GetLongitudePropertyName() {
-		return longitude_property_name;
-	}
-
-	public String GetLatitudePropertyName() {
-		return latitude_property_name;
-	}
-
-	public String GetRMBR_minx_name()
-	{
-		return RMBR_minx_name;
-	}
-
-	public String GetRMBR_miny_name()
-	{
-		return RMBR_miny_name;
-	}
-
-	public String GetRMBR_maxx_name()
-	{
-		return RMBR_maxx_name;
-	}
-
-	public String GetRMBR_maxy_name()
-	{
-		return RMBR_maxy_name;
-	}
-
-	public String GetGeoB_name()
-	{
-		return GeoB_name;
-	}
-
-	public String GetBitmap_name()
-	{
-		return bitmap_name;
-	}
-
-	public int GetMergeRatio()
-	{
-		return merge_ratio;
-	}
-
-	public double GetMax_RMBR_Ratio()
-	{
-		return Max_RMBR_ratio;
-	}
-
-	public int GetMax_ReachGrid()
-	{
-		return Max_ReachGrid;
-	}
+public class Config { public Config() {}
+  private String SERVER_ROOT_URI = "http://localhost:7474/db/data";
+  
+  private String longitude_property_name = "longitude";
+  private String latitude_property_name = "latitude";
+  
+  private String RMBR_minx_name = "RMBR_minx";
+  private String RMBR_miny_name = "RMBR_miny";
+  private String RMBR_maxx_name = "RMBR_maxx";
+  private String RMBR_maxy_name = "RMBR_maxy";
+  
+  private String GeoB_name = "GeoB";
+  private String bitmap_name = "Bitmap";
+  private int merge_ratio = 0;
+  private double Max_RMBR_ratio = 2.0D;
+  private int Max_ReachGrid = 200;
+  
+  public String GetServerRoot() {
+    return SERVER_ROOT_URI;
+  }
+  
+  public String GetLongitudePropertyName() {
+    return longitude_property_name;
+  }
+  
+  public String GetLatitudePropertyName() {
+    return latitude_property_name;
+  }
+  
+  public String GetRMBR_minx_name()
+  {
+    return RMBR_minx_name;
+  }
+  
+  public String GetRMBR_miny_name()
+  {
+    return RMBR_miny_name;
+  }
+  
+  public String GetRMBR_maxx_name()
+  {
+    return RMBR_maxx_name;
+  }
+  
+  public String GetRMBR_maxy_name()
+  {
+    return RMBR_maxy_name;
+  }
+  
+  public String GetGeoB_name()
+  {
+    return GeoB_name;
+  }
+  
+  public String GetBitmap_name()
+  {
+    return bitmap_name;
+  }
+  
+  public int GetMergeRatio()
+  {
+    return merge_ratio;
+  }
+  
+  public double GetMax_RMBR_Ratio()
+  {
+    return Max_RMBR_ratio;
+  }
+  
+  public int GetMax_ReachGrid()
+  {
+    return Max_ReachGrid;
+  }
 }
